e29d56bfa24eb38682bbdfd1e0b67d6cf9ea2022
NickDurnev <nikitaspec@gmail.com>
2024-07-25 23:10:06 +0300

    feat: initialize express server and mongodb connection

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const contactsDB = process.env.DB_HOST;
+
+const connection = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(contactsDB);
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // make the process fail
+    process.exit(1);
+  }
+};
+
+module.exports = connection;
